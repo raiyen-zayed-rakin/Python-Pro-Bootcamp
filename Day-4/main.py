f374ae4ba9fcb950913b1d96c5e570aa7f828633
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-07-31 04:39:32 +0600

    new

---
+++ Day-4/main.py
@@ -0,0 +1,25 @@
+import random
+
+
+# print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+
+
+# if num == num1:
+#     print("tie")
+# elif num == "0" and num1 == "1":
+#     print("You - Rock, Comm - Paper, COMM WINS")
+# elif num == "0" and num1 == "2":
+#     print("You - Rock, Comm - Scissors, YOU WINS")
+# elif num == "1" and num1 == "0":
+#     print("You - Paper, Comm - Rock, YOU WINS")
+# elif num == "1" and num1 == "2":
+#     print("You - Paper, Comm - Scissors, COMM WINS")
+# elif num == "2" and num1 == "0":
+#     print("You - Scissors, Comm - Rock, COMM WINS")
+# elif num == "2" and num1 == "1":
+#     print("You - Scissors, Comm - Paper, YOU WIN")
+
+
+
+
+
