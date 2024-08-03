ec163a9cb3884639d8c4c3afdbf82a5d51c2cf7f
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-08-03 14:16:11 +0600

    Guessing Game

--- Day-12-Number_Guessing_Game/main.py
+++ Day-12-Number_Guessing_Game/main.py
@@ -1,6 +1,9 @@
 
 import art
-import random
+from random import randint
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
 print(art.logo)
 print("Welcome to the Number Guessing Game!")
@@ -10,17 +13,15 @@ mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
 
 def game(game_mode):
-    my_nums = []
-    for number in range(1, 101):
-        my_nums.append(number)
-    guess = random.choice(my_nums)
-
-    while game_mode != 0:
+    guess = randint(1,100)
+    print(guess)
+    user_guess = 0
+    while guess != user_guess or game_mode != 0:
         print(f"You have {game_mode} attempts remaining to guess the number.")
         user_guess = int(input("Make a guess: "))
         if user_guess == guess:
             print(f"You got it! The answer was {user_guess}")
-        elif guess - user_guess >= 10:
+        elif guess > user_guess:
             print("Too low")
             print("Try again")
             game_mode -= 1
@@ -28,12 +29,15 @@ def game(game_mode):
             print("Too high")
             print("Try again")
             game_mode -= 1
+        if game_mode == 0:
+            print("you have ran out of guesses, You have lost.")
+            return
 
 
 if mode == "easy":
-    game(10)
+    game(EASY_LEVEL_TURNS)
 elif mode == "hard":
-    game(5)
+    game(HARD_LEVEL_TURNS)
 else:
     print("Wrong input")
 
