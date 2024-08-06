46c6f5ebb6a2f82d5f2c79db0b4d2dc09ff246da
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-08-06 13:28:02 +0600

    The fookin coffee OOP machine

---
+++ Day-16-OOP_Coffee_Machine/main.py
@@ -0,0 +1,20 @@
+# from turtle import Turtle
+# from turtle import Screen
+#
+#
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("red", "green")
+# timmy.forward(100)
+# timmy.right(90)
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+#
+# from menu import Menu, MenuItem
+# from coffee_maker import CoffeeMaker
+# from money_machine import MoneyMachine
+#
