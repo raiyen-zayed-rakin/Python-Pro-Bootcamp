c638f0e5e5046f193a60a1a283062f32c0da515e
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-08-06 23:38:58 +0600

    a big fookin leap

--- Day-16-OOP_Coffee_Machine/main.py
+++ Day-16-OOP_Coffee_Machine/main.py
@@ -1,17 +1,17 @@
-# from turtle import Turtle
-# from turtle import Screen
-#
-#
-# timmy = Turtle()
-# timmy.shape("turtle")
-# timmy.color("red", "green")
-# timmy.forward(100)
-# timmy.right(90)
-# timmy.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+from turtle import Turtle
+from turtle import Screen
+
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red", "green")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
 
 #
 # from menu import Menu, MenuItem
---
+++ Day-18-Turtle_Graphics_Art/main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+
+timmy_the_turtle = Turtle()
+
+timmy_the_turtle.shape("turtle")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
