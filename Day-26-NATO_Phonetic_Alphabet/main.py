c30587962eef5845159d1da99d295b289bce7ec4
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-08-15 14:11:17 +0600

    NATO Phonetic Alphabet

--- Day-26-NATO_Phonetic_Alphabet/main.py
+++ Day-26-NATO_Phonetic_Alphabet/main.py
@@ -1,4 +1,48 @@
-list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
-numbers = [int(letter) for letter in list_of_strings]
-result = [number for number in numbers if number % 2 == 0]
-print(result)
+# list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
+# numbers = [int(letter) for letter in list_of_strings]
+# result = [number for number in numbers if number % 2 == 0]
+# print(result)
+# weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+#
+# weather_f = {day : ((temp_c * 9/5) + 32)  for (day, temp_c) in weather_c.items()}
+#
+# print(weather_f)
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# list_of_words = sentence.split()
+# result = {word : len(word) for word in list_of_words}
+# print(result)
+#
+#For Loop
+numbers = [1, 2, 3]
+new_list = []
+for n in numbers:
+    add_1 = n + 1
+    new_list.append(add_1)
+
+#List Comprehension
+new_list = [n + 1 for n in numbers]
+
+#String as List
+name = "Angela"
+letters_list = [letter for letter in name]
+
+#Range as List
+range_list = [n * 2 for n in range(1, 5)]
+
+#Conditional List Comprenhension
+names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
+short_names = [name for name in names if len(name) < 5]
+
+upper_case_names = [name.upper() for name in names if len(name) > 4]
+
+#Dictionary Comprehension
+import random
+student_grades = {name: random.randint(1, 100) for name in names}
+print(student_grades)
+
+passed_students = {
+    student: grade
+    for (student, grade) in student_grades.items() if grade >= 60
+}
+print(passed_students)
+
