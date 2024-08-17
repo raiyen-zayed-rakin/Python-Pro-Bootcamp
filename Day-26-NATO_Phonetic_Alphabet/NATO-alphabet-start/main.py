761b2b28f839a31b38ed9ba21d8a89b5b50d69a2
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-08-18 00:34:24 +0600

    Updated exception and try/except

--- Day-26-NATO_Phonetic_Alphabet/NATO-alphabet-start/main.py
+++ Day-26-NATO_Phonetic_Alphabet/NATO-alphabet-start/main.py
@@ -24,13 +24,24 @@ import pandas
 
 # Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
+# Can be done with while loop too.
+
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
 # more fekad up
 # new_list = [code for (letter, code) in phonetic_dict.items() if letter in user_input]
-print(output_list)
