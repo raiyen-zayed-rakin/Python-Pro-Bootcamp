85f4d40b9ebe2ee1d8ca5d8fe886e3f21172d96f
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-08-22 04:37:01 +0600

    email sender

--- Day-32-Automated_Birthday_Wisher/main.py
+++ Day-32-Automated_Birthday_Wisher/main.py
@@ -1,36 +1,31 @@
-# import smtplib
-#
-# my_email = "rakinudemy@gmail.com"
-# password = "ffnfxonbgemldmnp"
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="rakinudemy@yahoo.com",
-#         msg="Subject:Hello\n\nThis is the body of my email"
-#     )
-
-import datetime as dt
+from datetime import datetime
 import random
+import pandas
 import smtplib
-now = dt.datetime.now()
-weekday = now.weekday()
 
-my_email = "rakinudemy@gmail.com"
-password = "ffnfxonbgemldmnp"
+MY_EMAIL = "rakinudemy@gmail.com"
+MY_PASSWORD = "ffnfxonbgemldmnp"
 
-if weekday == 3:
-    with open("quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
+today = (datetime.now().month, datetime.now().day)
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row.month, data_row.day) : data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_files:
+
+        contents = letter_files.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(user=my_email, password=password)
+        connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
-            from_addr=my_email,
-            to_addrs="rakinudemy@yahoo.com",
-            msg=f"Subject:Quote of the day.\n\n{quote}"
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
+
+
