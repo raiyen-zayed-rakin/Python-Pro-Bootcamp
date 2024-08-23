846f692af68880e74479172e5929835aef9b9f08
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-08-24 04:27:04 +0600

    Quizzler

--- Day-34-Quizzler_App/main.py
+++ Day-34-Quizzler_App/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -11,9 +12,18 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+# print("You've completed the quiz")
+# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+# Typehints and arrows:
+# def police_check(age: int) -> bool:
+#     if age > 18:
+#         drive = True
+#     else:
+#         drive = False
+#     return drive
