39732d0f08659b4877ed40d3833ed800510280b7
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-09-05 20:32:10 +0600

    new

--- Day-45-100_Must_Watch_Movies/bs4-start/main.py
+++ Day-45-100_Must_Watch_Movies/bs4-start/main.py
@@ -1,15 +1,41 @@
 from bs4 import BeautifulSoup
+import requests
 import lxml
 
-with open("website.html") as file:
-    contents = file.read()
+# with open("website.html") as file:
+#     contents = file.read()
+#
+#
+# soup = BeautifulSoup(contents, "html.parser")
+#
+# # print(soup.title)
+# # print(soup.title.name)
+# # print(soup.title.string)
+#
+# # print(soup.find_all(name="a"))
+# # print(soup.find_all(name="p").)
+#
+# all_anchor_tags = soup.find_all(name="a")
+#
+# for tag in all_anchor_tags:
+#     # print(tag.get_text())
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.getText())
+#
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
+#
+# print(soup.select(selector=".heading"))
 
+response1 = requests.get(url="https://appbrewery.github.io/news.ycombinator.com/")
+response = requests.get(url="https://news.ycombinator.com/news")
 
-soup = BeautifulSoup(contents, "html.parser")
+yc_webpage = response.text
 
-# print(soup.title)
-# print(soup.title.name)
-# print(soup.title.string)
-
-print(soup.find_all(name="a"))
-print(soup.find_all(name="p"))
+soup = BeautifulSoup(yc_webpage, "html.parser")
+print(soup.select_one(selector=".titleline a").getText())
