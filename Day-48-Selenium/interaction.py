566b8f10f52ee6b1210fd7a5fb79c84d08495340
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-09-20 02:02:39 +0600

    selenium

--- Day-48-Selenium/interaction.py
+++ Day-48-Selenium/interaction.py
@@ -1,11 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
-article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount")
-print(article_count.text)
-driver.quit()
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+# driver.get("https://www.stackoverflow.com/")
+driver.get("http://secure-retreat-92358.herokuapp.com/")
+# article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+
+# article_count.click()
+# all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+
+# search = driver.find_element(By.NAME, value="q")
+# search.send_keys("Python", Keys.ENTER)
+
+# Sending keyboard input to Selenium
+# search.send_keys(Keys.ENTER)
+
+first_name = driver.find_element(By.NAME, value="fName")
+last_name = driver.find_element(By.NAME, value="lName")
+email = driver.find_element(By.NAME, value="email")
+
+first_name.send_keys("Raiyen Zayed")
+last_name.send_keys("Rakin")
+email.send_keys("rakin69@gmail.com")
+
+signup_btn = driver.find_element(By.CLASS_NAME, value="btn")
+signup_btn.click()
+
+
+
+#driver.quit()
